7714ced15d71bd5d57d21b30df82bf11b42fa0b1
tanmoy-bms <53893576+tanmoy-bms@users.noreply.github.com>
2020-03-13 14:45:50 +0530

    Create MainActivity.java

---
+++ Lab2/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.myapplication1;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import android.os.Bundle;
+
+
+
+import android.content.Intent;
+
+
+
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import android.widget.Spinner;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    //Defining the Views
+
+    EditText e1,e2;
+
+    Button bt;
+
+    Spinner s;
+
+
+
+    //Data for populating in Spinner
+
+    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
+
+
+
+    String name,reg,dept;
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+        //Referring the Views
+
+        e1= (EditText) findViewById(R.id.editText);
+
+        e2= (EditText) findViewById(R.id.editText2);
+
+
+
+        bt= (Button) findViewById(R.id.button);
+
+
+
+        s= (Spinner) findViewById(R.id.spinner);
+
+
+
+        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+
+        s.setAdapter(adapter);
+
+
+
+        bt.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override public void onClick(View v) {
+
+
+
+                name=e1.getText().toString();
+
+                reg=e2.getText().toString();
+
+                dept=s.getSelectedItem().toString();
+
+
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+
+
+
+
+
+                i.putExtra("name_key", name);
+
+                i.putExtra("reg_key",reg);
+
+                i.putExtra("dept_key", dept);
+
+
+
+                startActivity(i);
+
+
+
+            }
+
+        });
+
+    }
+
+}
