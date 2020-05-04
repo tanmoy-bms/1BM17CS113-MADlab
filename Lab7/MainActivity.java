2bebcfd814beb05a08b615da2c573ed9e96cf70c
tanmoy-bms <53893576+tanmoy-bms@users.noreply.github.com>
2020-05-04 17:24:50 +0530

    Update MainActivity.java

--- Lab7/MainActivity.java
+++ Lab7/MainActivity.java
@@ -1,7 +1,7 @@
-package com.example.lp7;
+package com.example.lab7;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
@@ -10,43 +10,34 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
     }
 
-    public void onClick_getLocation(View v) {
-        LocationManager locationManager;
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        assert locationManager != null;
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
-    }
 
     @Override
     public void onLocationChanged(Location location) {
 
-        Log.d("bmsce","change");
-        String msg="New Latitude: "+location.getLatitude()+"New Longitude: "+location.getLongitude();
-        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_LONG).show();
+        String msg = "New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
@@ -56,13 +47,38 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onProviderEnabled(String provider) {
-        Toast.makeText(getBaseContext(),"GPS is turned on!!",Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(MainActivity.this, "GPS is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
         startActivity(intent);
-        Toast.makeText(getBaseContext(),"GPS is turned of!!!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "GPS is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onClick_getLocation(View view) {
+
+        LocationManager locationManager;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
+
     }
 }
